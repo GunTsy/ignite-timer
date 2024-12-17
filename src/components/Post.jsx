a12a3b015b944b5aeceeacf1a057970c4fd5280d
GunTsy <carlosalexandre3254@gmail.com>
2024-12-17 13:24:04 -0300

    finish layout project

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
   <article className={styles.post}>
     <header>
       <div className={styles.author}>
-        <img className={styles.avatar} src="https://github.com/guntsy.png" />
+        <Avatar src="https://github.com/DAVI-REZENDE.png" />
         <div className={styles.authorInfo}>
           <strong>Alexandre</strong>
           <span>Web Developer</span>
@@ -37,6 +39,12 @@ export function Post() {
         <button type='submit'>Publicar</button>
       </footer>
     </form>
+
+    <div className={styles.commnetList}>
+      <Comment />
+      <Comment />
+      <Comment />
+    </div>
   </article>
  )
 }
