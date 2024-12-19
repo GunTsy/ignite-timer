34c49ff2e60709939d9245c26972800056d3729f
GunTsy <carlosalexandre3254@gmail.com>
2024-12-19 17:49:41 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,40 @@ import { Sidebar } from "./components/Sidebar"
 import styles from './App.module.css'
 import './global.css'
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/guntsy.png',
+      name: 'Carlos Alexandre',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 ' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' 
+      },        
+    ],
+    publishedAt: new Date('2024-12-10 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/DAVI-RESENDE.png',
+      name: 'Davi',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 ' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' 
+      },        
+    ],
+    publishedAt: new Date('2024-12-20 22:00:00')
+  },
+]
+
+
 export function App() {
   return (
     <>
@@ -12,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Carlos Alexandre" 
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic voluptatibus nihil qui cupiditate illo facere quidem autem."
-          />
-          <Post 
-            author="Carlos Alexandre" 
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic voluptatibus nihil qui cupiditate illo facere quidem autem."
-          />
+         {post.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+         })}
         </main>
       </div>
     </>
