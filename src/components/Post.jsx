99b60070864777b3c1029a12e7d9236493b1980a
GunTsy <carlosalexandre3254@gmail.com>
2024-12-20 16:30:02 -0300

    creating the logic of the delete comment function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,11 @@ export function Post({author, publishedAt, content}) {
     setCommentText(event.target.value)
   }
 
+
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`);
+  }
+
  return (
   <article className={styles.post}>
     <header>
@@ -52,9 +57,9 @@ export function Post({author, publishedAt, content}) {
     <div className={styles.content}>
        {content.map(line => {
         if(line.type === 'paragraph') {
-          return <p>{line.content}</p>
+          return <p key={line.content}>{line.content}</p>
         } else if (line.type === 'link') {
-          return <p><a href="#">{line.content}</a></p>
+          return <p key={line.content}><a href="#">{line.content}</a></p>
         }
        })}  
     </div>
@@ -76,7 +81,7 @@ export function Post({author, publishedAt, content}) {
 
     <div className={styles.commnetList}>
       {comment.map(comment => {
-        return <Comment content={comment} />
+        return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
       })}
     </div>
   </article>
